4a43dee3982695283a35d4faf6f5e5e398f21e6a
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-26 21:05:35 +0800

    Add gradient background container

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
-
+import Head from "next/head";
+import { GradientBackgroundContainer } from "@/components/QuoteGenerator/QuoteGeneratorElements";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Inspirational Quote Image Generator</title>
-        <meta name="description" content="Generate an image with inspirational quote." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={`${styles.main}`}>
-      </main>
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<title>Inspirational Quote Image Generator</title>
+				<meta
+					name="description"
+					content="Generate an image with inspirational quote."
+				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<GradientBackgroundContainer></GradientBackgroundContainer>
+		</>
+	);
 }
